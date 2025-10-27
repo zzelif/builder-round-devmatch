c54c3bbe1d756ce6bda2d884492d675ece84844b
dan <dan.lettac@gmail.com>
2025-10-27 21:26:03 +0800

    basic app setup with navigation and routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TopNav from "@/components/navbar/TopNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TopNav />
         {children}
       </body>
     </html>
---
+++ src/app/network/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NetworkPage() {
+  return <div>MessagePage</div>;
+}
